70c0cd3585ad7b8994ae06c4b11c7b782c479d90
David <david@davidpineiro.xyz>
2022-09-21 01:29:42 -0400

    fix maybe

--- app/index.js
+++ app/index.js
@@ -176,9 +176,9 @@ app.get('/freeram', (req,res) =>{
 var http = express();
 // set up a route to redirect http to https
 http.get('*', function(req, res) {  
-    res.redirect('https://' + req.headers.host + req.url);
+    // res.redirect('https://' + req.headers.host + req.url);
     // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
-    // res.redirect('https://example.com' + req.url);
+    res.redirect('https://davidpineiro.xyz' + req.url);
 })
 // have it listen on 8080
 http.listen(80);
