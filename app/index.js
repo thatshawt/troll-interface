08954a90e328d4b76f921999d17a32238496c427
David <david@davidpineiro.xyz>
2022-09-23 19:04:09 -0400

    almost...

--- app/index.js
+++ app/index.js
@@ -72,8 +72,9 @@ app.use((req,res,next) => {
 
 app.use(csp({
   directives: {
-    defaultSrc: [`'self'`,'fonts.googleapis.com'],
-    scriptSrc: [`'self'`, "*.googletagmanager.com",(req, res) => `'nonce-${ res.locals.nonce }'`],
+    defaultSrc: [`'self'`,'fonts.googleapis.com',
+              "*.google-analytics.com", "*.googletagmanager.com"],
+    scriptSrc: [`'self'`,(req, res) => `'nonce-${ res.locals.nonce }'`],
     fontSrc:["'self'",'fonts.gstatic.com', "fonts.cdnfonts.com", "cdnjs.cloudflare.com"],
     styleSrc: ["'self'", "fonts.cdnfonts.com", "cdnjs.cloudflare.com"]
   }
